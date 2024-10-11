6363f8164ce65acde0ed33f2a10611ecc955aa3b
Jabeen <jabeentaj1531@gmail.com>
2024-10-11 06:37:44 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className='bgContainer'>
+    <h1 className='heading'>Congratulations</h1>
+    <div class='card-container'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h2 className='heading'>Kiran V</h2>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
